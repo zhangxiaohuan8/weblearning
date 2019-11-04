e98708072cab93036bd44be87ceaedec7b521746
zhangxiaohuan8 <2916244782@qq.com>
2019-11-04 08:20:13 +0800

    add

---
+++ precache-manifest.5a7f74e916f1fcd5658f2593fe05a97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5abfb723a1ec9fcef69272e62ad7f8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef250bed4a44fb3e36af",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "5a4e2222450fa185fcda",
+    "url": "/static/css/main.9241492f.chunk.css"
+  },
+  {
+    "revision": "ef250bed4a44fb3e36af",
+    "url": "/static/js/2.9966803a.chunk.js"
+  },
+  {
+    "revision": "5a4e2222450fa185fcda",
+    "url": "/static/js/main.e10e5d2e.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
